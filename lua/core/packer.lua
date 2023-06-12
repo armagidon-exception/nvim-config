a2db0e29064f1dae8e3e14a2b36ab6034cbf41d8
Armagidon <yt.armagidon.mc@gmail.com>
2023-06-12 15:09:25 +0300

    Finished rebuilding

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -14,36 +14,35 @@ local packer_bootstrap = ensure_packer()
 local packer_config = {
     display = {
         open_fn = function()
-            local result, win, buf = require('packer.util').float {
+            local result, win, buf = require("packer.util").float {
                 border = {
-                    { '╭', 'FloatBorder' },
-                    { '─', 'FloatBorder' },
-                    { '╮', 'FloatBorder' },
-                    { '│', 'FloatBorder' },
-                    { '╯', 'FloatBorder' },
-                    { '─', 'FloatBorder' },
-                    { '╰', 'FloatBorder' },
-                    { '│', 'FloatBorder' },
+                    { "╭", "FloatBorder" },
+                    { "─", "FloatBorder" },
+                    { "╮", "FloatBorder" },
+                    { "│", "FloatBorder" },
+                    { "╯", "FloatBorder" },
+                    { "─", "FloatBorder" },
+                    { "╰", "FloatBorder" },
+                    { "│", "FloatBorder" },
                 },
             }
-            vim.api.nvim_win_set_option(win, 'winhighlight', 'NormalFloat:Normal')
+            vim.api.nvim_win_set_option(win, "winhighlight", "NormalFloat:Normal")
             return result, win, buf
         end,
     },
 }
 
-
 local function install_plugins(use)
-    use 'wbthomason/packer.nvim'
+    use "wbthomason/packer.nvim"
 
     -- Color scheme
-    use 'folke/tokyonight.nvim'
-    
+    use "folke/tokyonight.nvim"
+
     -- Status line
-    use 'nvim-lualine/lualine.nvim'
-    
+    use "nvim-lualine/lualine.nvim"
+
     -- Icons
-    use 'nvim-tree/nvim-web-devicons'
+    use "nvim-tree/nvim-web-devicons"
 
     -- Projects
     use "ahmedkhalf/project.nvim"
@@ -52,58 +51,63 @@ local function install_plugins(use)
     use "akinsho/toggleterm.nvim"
 
     -- Greeter
-    use 'goolord/alpha-nvim'
+    use "goolord/alpha-nvim"
 
     -- Bufferline
-    use 'akinsho/bufferline.nvim'
+    use "akinsho/bufferline.nvim"
 
     -- Telescope
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-lua/plenary.nvim'
+    use "nvim-telescope/telescope.nvim"
+    use "nvim-lua/plenary.nvim"
     use "nvim-telescope/telescope-file-browser.nvim"
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-    use 'nvim-telescope/telescope-ui-select.nvim'
-    use 'nvim-telescope/telescope-symbols.nvim'
+    use {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        run =
+        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    }
+    use "nvim-telescope/telescope-ui-select.nvim"
+    use "nvim-telescope/telescope-symbols.nvim"
 
     -- Treesitter
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'nvim-treesitter/playground'
+    use "nvim-treesitter/nvim-treesitter"
+    use "nvim-treesitter/playground"
 
     -- Surround
     use "kylechui/nvim-surround"
     -- Commentary
-    use 'JoosepAlviste/nvim-ts-context-commentstring'
-    use 'numToStr/Comment.nvim'
+    use "JoosepAlviste/nvim-ts-context-commentstring"
+    use "numToStr/Comment.nvim"
     -- Autopairs
- 	use "windwp/nvim-autopairs"
+    use "windwp/nvim-autopairs"
 
     -- Autotags
-    use 'windwp/nvim-ts-autotag'
+    use "windwp/nvim-ts-autotag"
 
     -- Which key
     use "folke/which-key.nvim"
 
     -- LSP
-    use 'neovim/nvim-lspconfig' -- Collection of lsp configs
-    use "williamboman/mason.nvim" -- LSP package manager
+    use "neovim/nvim-lspconfig"          -- Collection of lsp configs
+    use "williamboman/mason.nvim"        -- LSP package manager
     use "williamboman/mason-lspconfig.nvim" -- Hook for Mason and lsp config
-    use 'hrsh7th/cmp-nvim-lsp' -- Completion
-    use 'hrsh7th/cmp-buffer' -- Completion source for buffer
-    use 'hrsh7th/cmp-path' -- Completion source for file system
-    use 'hrsh7th/cmp-cmdline' -- Completion source for commands
-    use 'hrsh7th/nvim-cmp' --  Completion
-    use 'jose-elias-alvarez/null-ls.nvim' -- hooking into lsp
-    use "jay-babu/mason-null-ls.nvim" -- Bridge for mason and null-ls
-    use 'dcampos/nvim-snippy' -- Snippets
-    use 'dcampos/cmp-snippy' -- Snippets
-    use 'folke/neodev.nvim' -- For neovim docs
-    use "folke/neoconf.nvim" -- Json configuration for lua_ls
-    use "folke/trouble.nvim" -- Pretty diagnostics
-    use "onsails/lspkind.nvim" -- Kind Icons
+    use "hrsh7th/cmp-nvim-lsp"           -- Completion
+    use "hrsh7th/cmp-buffer"             -- Completion source for buffer
+    use "hrsh7th/cmp-path"               -- Completion source for file system
+    use "hrsh7th/cmp-cmdline"            -- Completion source for commands
+    use "hrsh7th/nvim-cmp"               --  Completion
+    use "jose-elias-alvarez/null-ls.nvim" -- hooking into lsp
+    use "jay-babu/mason-null-ls.nvim"    -- Bridge for mason and null-ls
+    use "dcampos/nvim-snippy"            -- Snippets
+    use "dcampos/cmp-snippy"             -- Snippets
+    use "folke/neodev.nvim"              -- For neovim docs
+    use "folke/neoconf.nvim"             -- Json configuration for lua_ls
+    use "folke/trouble.nvim"             -- Pretty diagnostics
+    use "onsails/lspkind.nvim"           -- Kind Icons
+    use "ray-x/lsp_signature.nvim"       -- Method signature
 
     if packer_bootstrap then
-        require('packer').sync()
+        require("packer").sync()
     end
 end
 
-return require('packer').startup({install_plugins, config = packer_config})
+return require("packer").startup { install_plugins, config = packer_config }
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -114,6 +114,11 @@ _G.packer_plugins = {
     path = "/home/armagidon/.local/share/nvim/site/pack/packer/start/cmp-snippy",
     url = "https://github.com/dcampos/cmp-snippy"
   },
+  ["lsp_signature.nvim"] = {
+    loaded = true,
+    path = "/home/armagidon/.local/share/nvim/site/pack/packer/start/lsp_signature.nvim",
+    url = "https://github.com/ray-x/lsp_signature.nvim"
+  },
   ["lspkind.nvim"] = {
     loaded = true,
     path = "/home/armagidon/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
