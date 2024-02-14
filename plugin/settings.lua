5896aded3f97cacc17a4f6472d27519a7b771a9a
Armagidon <yt.armagidon.mc@gmail.com>
2024-02-14 19:14:43 +0300

    Changed filetype associations for *.h header files

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -2,12 +2,12 @@ local opt = vim.opt
 local g = vim.g
 opt.autoread = true
 --opt.bsdir = 'buffer'
-opt.clipboard:append({'unnamed', 'unnamedplus'})
+opt.clipboard:append { "unnamed", "unnamedplus" }
 opt.cursorcolumn = true
 opt.cursorline = true
 opt.expandtab = true
-opt.fileencoding = 'utf-8'
-opt.mouse = 'a'
+opt.fileencoding = "utf-8"
+opt.mouse = "a"
 opt.number = true
 opt.relativenumber = true
 opt.autoindent = true
@@ -30,17 +30,23 @@ opt.incsearch = true
 opt.termguicolors = true
 opt.scrolloff = 10
 opt.updatetime = 50
-opt.signcolumn = 'yes'
+opt.signcolumn = "yes"
 
 opt.hidden = true
 
-vim.opt.completeopt:append( { "menu", "menuone", "noselect" } )
+vim.opt.completeopt:append { "menu", "menuone", "noselect" }
 
 if vim.g.neovide then
-    vim.o.guifont = "Source Code Pro:h10"
+	vim.o.guifont = "Source Code Pro:h10"
 end
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_python_provider = 0
 vim.g.loaded_python3_provider = 0
+
+vim.filetype.add {
+	extension = {
+		h = "c",
+	},
+}
