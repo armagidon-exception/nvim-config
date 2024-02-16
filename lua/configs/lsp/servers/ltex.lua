08930ffc62f2ee5f75e501212cd355c3dca34172
Armagidon <yt.armagidon.mc@gmail.com>
2024-02-17 01:08:44 +0300

    cleanup of ltex server

--- lua/configs/lsp/servers/ltex.lua
+++ lua/configs/lsp/servers/ltex.lua
@@ -1,53 +1,42 @@
+local mappings = {}
+
+function mappings.markdown(bufnr)
+	return {
+		{
+			mode = "v",
+			keys = "gbo",
+			command = "<Plug>(nvim-surround-visual)mbold<cr>",
+			opts = {
+				buffer = bufnr,
+				desc = "Make bold",
+			},
+		},
+		{
+			mode = "v",
+			keys = "gbi",
+			command = "<Plug>(nvim-surround-visual)mitalic<cr>",
+			opts = {
+				buffer = bufnr,
+				desc = "Make italic",
+			},
+		},
+		{
+			mode = "v",
+			keys = "<leader>ico",
+			command = "<Plug>(nvim-surround-visual)mcode<cr>",
+			opts = {
+				buffer = bufnr,
+				desc = "Insert code",
+			},
+		},
+	}
+end
+
 return {
 	mappings = function(bufnr)
-		if vim.bo.filetype == "markdown" then
-			return {
-				{
-					mode = "v",
-					keys = "gbo",
-					command = "<Plug>(nvim-surround-visual)mbold<cr>",
-					opts = {
-						buffer = bufnr,
-						desc = "Make bold",
-					},
-				},
-				{
-					mode = "n",
-					keys = "gbo",
-					command = "<Plug>(nvim-surround-normal)mbold<cr>",
-					opts = {
-						buffer = bufnr,
-						desc = "Make bold",
-					},
-				},
-				{
-					mode = "n",
-					keys = "gbi",
-					command = "<Plug>(nvim-surround-normal)mitalic<cr>",
-					opts = {
-						buffer = bufnr,
-						desc = "Make italic",
-					},
-				},
-				{
-					mode = "v",
-					keys = "gbi",
-					command = "<Plug>(nvim-surround-visual)mitalic<cr>",
-					opts = {
-						buffer = bufnr,
-						desc = "Make italic",
-					},
-				},
-				{
-					mode = "v",
-					keys = "<leader>ico",
-					command = "<Plug>(nvim-surround-visual)mcode<cr>",
-					opts = {
-						buffer = bufnr,
-						desc = "Insert code",
-					},
-				},
-			}
+		local maps = mappings[vim.bo.filetype]
+		if maps then
+			return maps(bufnr)
 		end
 		return {}
 	end,
