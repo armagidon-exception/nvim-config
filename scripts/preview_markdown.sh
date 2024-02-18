938e7d4affb4db4ab8bf9ebe53ae8fb05ae1d632
Armagidon <yt.armagidon.mc@gmail.com>
2024-02-18 23:35:42 +0300

    Cleaned up preview_markdown.sh

--- scripts/preview_markdown.sh
+++ scripts/preview_markdown.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-INPUT_FILE=$(mktemp -u)
-pandoc --from markdown+pipe_tables --pdf-engine=xelatex -V geometry:margin=1cm -V mainfont="JetBrains Mono" -i "$1" -t pdf -o "$INPUT_FILE" && atril "$INPUT_FILE"
+if [ -z "$1" ]; then
+    echo "Error: No file provided"
+    exit 1
+fi
+INPUT_FILE="$1"
+OUTPUT_FILE=$(mktemp -u)
+FONT="JetBrains Mono"
+
+pandoc --from markdown+pipe_tables \
+--pdf-engine=xelatex \
+-V geometry:margin=1cm \
+-V mainfont="$FONT" \
+-i "$INPUT_FILE" \
+-t pdf \
+-o "$OUTPUT_FILE" || exit
+
+atril "$OUTPUT_FILE"
