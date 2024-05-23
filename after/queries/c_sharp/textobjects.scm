82aeca9ee89cd508776c94f3f6ff648e5ce1839c
Armagidon <yt.armagidon.mc@gmail.com>
2024-05-23 22:09:36 +0300

    Fixed tree sitter text objects bug

--- after/queries/c_sharp/textobjects.scm
+++ after/queries/c_sharp/textobjects.scm
@@ -2,4 +2,4 @@
 
 (assignment_expression left:(_) @assignment.lhs right:(_) @assignment.rhs @assignment.inner) @assignment.outer
 
-(variable_declaration type:(_) (variable_declarator name:(_) @assignment.lhs (equals_value_clause _ @assignment.rhs)) @assignment.inner) @assignment.outer
+(variable_declaration type:(_) (variable_declarator name:(_) @assignment.lhs (_) @assignment.rhs) @assignment.inner) @assignment.outer
