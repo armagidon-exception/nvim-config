9229344cf8aae13e7f2304de59d0a2bfdcd08daa
Armagidon <yt.armagidon.mc@gmail.com>
2024-10-01 00:39:52 +0300

    Fix nvim surround

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -1,6 +1,15 @@
 return {
 	-- Surround
-	{ "kylechui/nvim-surround", event = { "BufNewFile", "BufReadPost" } },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup {
+				-- Configuration here, or leave empty to use defaults
+			}
+		end,
+	},
 
 	-- Commentary
 	{
@@ -35,7 +44,7 @@ return {
 
 	{
 		"michaelrommel/nvim-silicon",
-        cmd = "Silicon",
+		cmd = "Silicon",
 		opts = {
 			{
 				font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
