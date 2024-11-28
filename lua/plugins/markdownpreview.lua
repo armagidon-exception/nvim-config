f9602fb7dd1bff3b781c7d068b1c728d039c7a3a
Armagidon <yt.armagidon.mc@gmail.com>
2024-11-28 21:18:12 +0300

    Markpreview tweaks

--- lua/plugins/markdownpreview.lua
+++ lua/plugins/markdownpreview.lua
@@ -1,12 +1,28 @@
 return {
-	{
-		"OXY2DEV/markview.nvim",
-		lazy = false, -- Recommended
-		-- ft = "markdown" -- If you decide to lazy-load anyway
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false, -- Recommended
+        -- ft = "markdown" -- If you decide to lazy-load anyway
 
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            vim.api.nvim_create_autocmd({ "BufFilePost", "BufNew", "BufNewFile", "FileType" }, {
+                pattern = { "*" },
+                callback = function()
+                    if vim.bo.filetype ~= "markdown" then
+                        return
+                    end
+
+                    vim.keymap.set("n", "<leader>sp", "<cmd>Markview splitToggle<cr>",
+                        { buffer = vim.api.nvim_get_current_buf(), desc = "Toggle split mode" })
+                end
+            })
+            require 'markview'.setup {
+                hybrid_modes = { "n", "v", "c" }
+            }
+        end
+    },
 }
