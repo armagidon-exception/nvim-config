4914d5324b1b18048af8ed674eecec1df8c7e56b
Armagidon <yt.armagidon.mc@gmail.com>
2025-02-12 20:05:35 +0300

    Added actions-preview plugin

--- lua/armagidon/plugins/lsp.lua
+++ lua/armagidon/plugins/lsp.lua
@@ -7,4 +7,28 @@ return {
 		end,
 		lazy = true,
 	},
+	{
+		"aznhe21/actions-preview.nvim",
+		event = "LspAttach",
+		dependency = {
+			"nvim-telescope/telescope.nvim",
+		},
+		opts = {
+			telescope = require("telescope.themes").get_dropdown {},
+		},
+        init = function ()       
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    local map = function(keys, func, desc, mode)
+                        mode = mode or "n"
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+                    end
+                    map("<leader>ca", require("actions-preview").code_actions, "LSP code actions", { "n", "v" })
+                end,
+            })
+        end,
+		config = function(_, opts)
+			require("actions-preview").setup(opts)
+		end,
+	},
 }
