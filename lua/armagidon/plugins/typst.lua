66738fd012c91c23ac76d0e0d24d993576bc9462
Armagidon <yt.armagidon.mc@gmail.com>
2025-04-30 01:59:05 +0300

    Fixed main file searching for typst previewer

--- lua/armagidon/plugins/typst.lua
+++ lua/armagidon/plugins/typst.lua
@@ -6,7 +6,7 @@ return {
 	opts = {
 		dependencies_bin = {
 			websocat = "/usr/bin/websocat",
-			tinymist = vim.fn.expand("~/.local/share/nvim/mason/bin/tinymist"),
+			tinymist = vim.fn.expand "~/.local/share/nvim/mason/bin/tinymist",
 		},
 		get_root = function(path_of_main_file)
 			local root = os.getenv "TYPST_ROOT"
@@ -20,6 +20,20 @@ return {
 				end
 			end
 			return vim.fn.fnamemodify(path_of_main_file, ":p:h")
+		end,
+		get_main_file = function(main_buffer)
+			local dir = vim.fn.fnamemodify(main_buffer, ":p:h")
+			local clients = vim.lsp.get_clients { bufnr = 0 }
+			for _, client in ipairs(clients) do
+				if client.root_dir then
+					if dir == client.root_dir then
+						return main_buffer
+					else
+						return vim.split(vim.fn.glob(string.format("%s/*.typ", client.root_dir)), "\n")[1]
+					end
+				end
+			end
+			return main_buffer
 		end,
 	}, -- lazy.nvim will implicitly calls `setup {}`
 }
