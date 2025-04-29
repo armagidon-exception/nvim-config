0178bc319c1e64f205dbf90d23cf3519f8818efd
Armagidon <yt.armagidon.mc@gmail.com>
2025-04-29 20:47:17 +0300

    Removed unnecessary plugin

--- lua/armagidon/plugins/telescope.lua
+++ lua/armagidon/plugins/telescope.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"tsakirist/telescope-lazy.nvim",
-		dependencies = "nvim-telescope/telescope.nvim",
-		config = function()
-			require("telescope").load_extension "lazy"
-		end,
-	},
+	-- {
+	-- 	"tsakirist/telescope-lazy.nvim",
+	-- 	dependencies = "nvim-telescope/telescope.nvim",
+	-- 	config = function()
+	-- 		require("telescope").load_extension "lazy"
+	-- 	end,
+	-- },
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim" },
